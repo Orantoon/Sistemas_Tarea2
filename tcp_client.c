8a99b7cd6e068f03defb04192fb4cba1f3cc7c30
Deif <davidsuarez2001@gmail.com>
2023-01-06 19:57:17 -0600

    V1 Chat privado

--- tcp_client.c
+++ tcp_client.c
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 4444
 
@@ -16,10 +17,19 @@ char buffer[1024];
 int client_socket;
 
 char username [256];
+bool convPriv = false;
 
 void solUsername();
 bool menu();
 void nuevaConv();
+void esperarUsuario(char user[256]);
+void threadResp();
+void leerResp();
+
+void menuConv();
+void enviarMensaje();
+void enviarArchivo();
+
 void nuevoGrupo();
 void cantUsuarios();
 void listaUsuarios();
@@ -122,7 +132,111 @@ bool menu(){
 }
 
 void nuevaConv(){
-	printf("Nueva conversacion\n");
+	char user [256];
+	while (true){
+		printf("Escriba el Username de la persona con la que quisiera iniciar una nueva conversacion: ");
+		scanf("%s\n\n", user);
+		
+		send(client_socket, user, sizeof(user), 0);
+		
+		// Recibe la respuesta del servidor de si el usuario existe
+		recv(client_socket, buffer, 1024, 0);
+		if (strcmp(buffer, "Usuario Encontrado") == 0){
+			bzero(buffer, sizeof(buffer));
+			break;
+		}
+		printf("El usuario que desea contactar no se ha encontrado. Por favor, vuelva a intentarlo.\n");
+		bzero(buffer, sizeof(buffer));
+	}
+	
+	esperarUsuario(user);
+	
+	convPriv = true;
+	printf("\n--- CHAT CON %s ---\n\n", user);
+	menuConv();
+}
+
+void esperarUsuario(char user[256]){
+	printf("\n Esperando a que %s se conecte...\n\n", user);
+	while (true){
+		recv(client_socket, buffer, 1024, 0);
+		if (strcmp(buffer, "Conectado") == 0){
+			bzero(buffer, sizeof(buffer));
+			break;
+		}
+		bzero(buffer, sizeof(buffer));
+	}
+	
+	threadResp();
+	menuConv();
+}
+
+void threadResp(){
+	pthread_t id;
+	pthread_create(&id, NULL, leerResp, NULL);
+}
+
+void leerResp(){
+	char message[1024];
+	while (true){
+		recv(client_socket, message, 1024, 0);
+		
+		if (strcmp(message, "_EXIT_")){
+			printf("El usuario se ha desconectado, volviendo al menu principal ...\n\n");
+			break;
+		}
+		printf("<- %s\n\n", message);
+		bzero(message, sizeof(message));
+	}
+}
+
+void menuConv(){
+	bool salir = false;
+	int opcion;
+	
+	while (!salir && convPriv){
+		
+		printf("---------------------------------\n");
+		printf("Seleccione una accion a realizar:\n\n");
+
+		printf("1. Mandar un mensaje de texto.\n");
+		printf("2. Enviar un archivo.\n");
+		printf("3. Abandonar conversacion.\n\n");
+
+		scanf("%d", &opcion);
+		printf("\n\n");
+		
+		switch(opcion){
+			case 1:
+				enviarMensaje();
+				break;
+			case 2:
+				enviarArchivo();
+				break;
+			case 3:
+				strcpy(buffer, "_EXIT_");
+				send(client_socket, buffer, sizeof(buffer), 0);
+				bzero(buffer, sizeof(buffer));
+				salir = true;
+		}
+	}
+}
+
+void enviarMensaje(){
+	char mensaje [1024];
+	scanf("%s\n\n", mensaje);
+	
+	strcpy(buffer, "-> ");
+	strcat(buffer, username);
+	strcat(buffer, ": ");
+	strcat(buffer, mensaje);
+	
+	send(client_socket, buffer, sizeof(buffer), 0);
+	bzero(buffer, sizeof(buffer));
+}
+
+void enviarArchivo(){ // =================================================
+
 }
 
 void nuevoGrupo(){
--- tcp_server.c
+++ tcp_server.c
@@ -21,6 +21,7 @@ struct AcceptedSocket
 	int error;
 	bool acceptedSuccessfully;
 	char username[256];
+	int convPriv;	// socket del cliente con el que se hablara de forma privada
 };
 
 struct AcceptedSocket acceptedSockets[30];
@@ -29,10 +30,15 @@ int acceptedSocketsCount = 0;
 struct AcceptedSocket* acceptIncomingConnection(int serverSocketFD);
 void acceptNewClient(int serverSocketFD);
 void clientThread(struct AcceptedSocket *pSocket);
-void *optionRecv(int socketFD);
-void clientLeft(int socketFD);
+void optionRecv(struct AcceptedSocket *pSocket);
+void clientLeft(struct AcceptedSocket *pSocket);
 
-void nuevaConv();
+void nuevaConv(struct AcceptedSocket *pSocket);
+bool existeUser(char asking[256], char user[256]);
+int getSocket(char asking[256], char user[256]);
+void esperarUser(struct AcceptedSocket *pSocket);
+void chatPriv(struct AcceptedSocket *pSocket);
+
 void nuevoGrupo();
 void cantUsuarios(int socketFD);
 void listaUsuarios(int socketFD);
@@ -100,6 +106,7 @@ struct AcceptedSocket* acceptIncomingConnection(int serverSocketFD){
 	acceptedSocket->address = clientAddress;
 	acceptedSocket->acceptedSocketFD = clientSocketFD;
 	acceptedSocket->acceptedSuccessfully = clientSocketFD > 0;
+	acceptedSocket->convPriv = 0;
 	
 	// Get username
 	recv(clientSocketFD, buffer, 1024, 0);
@@ -128,13 +135,13 @@ void acceptNewClient(int serverSocketFD){
 
 void clientThread(struct AcceptedSocket *pSocket){
 	pthread_t id;
-	pthread_create(&id, NULL, optionRecv, pSocket->acceptedSocketFD);
+	pthread_create(&id, NULL, optionRecv, pSocket);
 }
 
-void *optionRecv(int socketFD){
+void optionRecv(struct AcceptedSocket *pSocket){
 	bool cerrar = false;
 	while (!cerrar){
-		recv(socketFD, buffer, 1024, 0);
+		recv(pSocket->acceptedSocketFD, buffer, 1024, 0);
 		
 
 		int opcion = atoi(buffer);
@@ -142,39 +149,117 @@ void *optionRecv(int socketFD){
 		
 		switch(opcion){
 			case 1:
-				nuevaConv();
+				nuevaConv(pSocket);
 				break;
 			case 2:
 				nuevoGrupo();
 				break;
 			case 3:
-				cantUsuarios(socketFD);
+				cantUsuarios(pSocket->acceptedSocketFD);
 				break;
 			case 4:
-				listaUsuarios(socketFD);
+				listaUsuarios(pSocket->acceptedSocketFD);
 				break;
 			case 5:
 				cerrar = true;
 		}
 	}
 	
-	clientLeft(socketFD);	
+	clientLeft(pSocket);	
 }
 
-void clientLeft(int socketFD){
+void clientLeft(struct AcceptedSocket *pSocket){
+	printf("El cliente %s se ha desconectado.", pSocket->username);
 	for (int i = 0; i < acceptedSocketsCount; i++){
-		if (acceptedSockets[i].acceptedSocketFD == socketFD){
-			acceptedSockets[i].acceptedSocketFD = 0;
-			strcpy (acceptedSockets[i].username, "");
+		if (acceptedSockets[i].acceptedSocketFD == pSocket->acceptedSocketFD){
+			acceptedSockets[i] = acceptedSockets[i + 1];
 		}
 	}
 	acceptedSocketsCount--;
 	
-	close(socketFD);
+	close(pSocket->acceptedSocketFD);
 }
 
-void nuevaConv(){
-	printf("Nueva conversacion\n");
+
+void nuevaConv(struct AcceptedSocket *pSocket){
+	char user[256];
+
+	while (true){
+		recv(pSocket->acceptedSocketFD, buffer, 1024, 0);
+
+		strcpy (user, buffer);
+		bzero(buffer, sizeof(buffer));
+		
+		if (existeUser(pSocket->username, user)){
+			strcpy (buffer, "Usuario Encontrado");
+			send(pSocket->acceptedSocketFD, buffer, strlen(buffer), 0);
+			bzero(buffer, sizeof(buffer));
+			break;
+		} else {
+			strcpy (buffer, "Usuario NO Encontrado");
+			send(pSocket->acceptedSocketFD, buffer, strlen(buffer), 0);
+			bzero(buffer, sizeof(buffer));
+		}
+	}
+	
+	pSocket->convPriv = getSocket(pSocket->username, user);
+	
+	esperarUser(pSocket);
+}
+
+bool existeUser(char asking[256], char user[256]){
+	for (int i = 0; i < 30; i++){
+		if (strcmp(acceptedSockets[i].username, asking) != 0 && strcmp(acceptedSockets[i].username, user) == 0){
+			return true;
+		}
+	}
+	return false;
+}
+
+int getSocket(char asking[256], char user[256]){
+	for (int i = 0; i < 30; i++){
+		if (strcmp(acceptedSockets[i].username, asking) != 0 && strcmp(acceptedSockets[i].username, user) == 0){
+			return acceptedSockets[i].acceptedSocketFD;
+		}
+	}
+	return -1;
+}
+
+void esperarUser(struct AcceptedSocket *pSocket){
+	bool salir = false;
+	while (!salir){
+		for (int i = 0; i < 30; i++){
+			if (acceptedSockets[i].acceptedSocketFD == pSocket->convPriv){
+				printf("%d\n\n",acceptedSockets[i].convPriv);
+				if (acceptedSockets[i].convPriv == pSocket->acceptedSocketFD){
+					printf("B\n\n");
+					salir = true;
+					break;
+				}
+			}
+		}
+	}
+	
+	strcpy (buffer, "Conectado");
+	send(pSocket->acceptedSocketFD, buffer, strlen(buffer), 0);
+	bzero(buffer, sizeof(buffer));
+	
+	chatPriv(pSocket);
+}
+
+void chatPriv(struct AcceptedSocket *pSocket){
+	while(true){
+		recv(pSocket->acceptedSocketFD, buffer, 1024, 0);
+		
+		if (strcmp(buffer, "_EXIT_") == 0){
+			send(pSocket->convPriv, buffer, strlen(buffer), 0);
+			bzero(buffer, sizeof(buffer));
+			break;
+		}
+		
+		send(pSocket->convPriv, buffer, strlen(buffer), 0);
+		bzero(buffer, sizeof(buffer));
+	}
 }
 
 void nuevoGrupo(){
